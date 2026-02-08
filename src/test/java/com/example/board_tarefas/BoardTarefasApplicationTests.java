c363e9eb8e49d9907a4e813949021fccee5bf660
TiagoCoder2022 <tiagonewalves@hotmail.com>
2026-02-07 22:35:19 -0300

    feat: Add BoardTarefasApplicationTests

---
+++ src/test/java/com/example/board_tarefas/BoardTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.board_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
