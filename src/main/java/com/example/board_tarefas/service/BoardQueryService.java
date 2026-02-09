e1a948dedac09d025a411da707f72067f08e157d
TiagoCoder2022 <tiagonewalves@hotmail.com>
2026-02-08 23:17:15 -0300

    refactor: Add explicity inicialization to BoardService dependences

--- src/main/java/com/example/board_tarefas/service/BoardQueryService.java
+++ src/main/java/com/example/board_tarefas/service/BoardQueryService.java
@@ -3,15 +3,21 @@ package com.example.board_tarefas.service;
 import com.example.board_tarefas.persistence.dao.BoardColumnDAO;
 import com.example.board_tarefas.persistence.dao.BoardDAO;
 import com.example.board_tarefas.persistence.entity.BoardEntity;
-import lombok.AllArgsConstructor;
+
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@AllArgsConstructor
-public class BoardQueryService {
 
+@Service
+public class BoardQueryService {
     private final BoardDAO boardDAO;
     private final BoardColumnDAO boardColumnDAO;
+
+    public BoardQueryService(BoardDAO boardDAO, BoardColumnDAO boardColumnDAO) {
+        this.boardDAO = boardDAO;
+        this.boardColumnDAO = boardColumnDAO;
+    }
 
     public Optional<BoardEntity> findById(Long id) {
         return boardDAO.findById(id)
