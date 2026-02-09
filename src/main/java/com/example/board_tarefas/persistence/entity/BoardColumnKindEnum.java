d3b345f9daf13dd8998eeeb47f2e7fbd850efc2a
TiagoCoder2022 <tiagonewalves@hotmail.com>
2026-02-08 23:16:00 -0300

    refactor: Add findByName method to BoardColumnKindEnum

--- src/main/java/com/example/board_tarefas/persistence/entity/BoardColumnKindEnum.java
+++ src/main/java/com/example/board_tarefas/persistence/entity/BoardColumnKindEnum.java
@@ -1,5 +1,13 @@
 package com.example.board_tarefas.persistence.entity;
 
+import java.util.stream.Stream;
+
 public enum BoardColumnKindEnum {
-    INITIAL, FINAL, CANCELED, PENDING
+    INITIAL, FINAL, CANCELED, PENDING;
+
+    public static BoardColumnKindEnum findByName(String name) {
+        return Stream.of(BoardColumnKindEnum.values())
+                .filter(b -> b.name().equals(name))
+                .findFirst().orElseThrow();
+    }
 }
