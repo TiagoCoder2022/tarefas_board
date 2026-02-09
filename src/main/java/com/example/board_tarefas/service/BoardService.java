c37d2adcc8f5978c944858f020ab4aaf1710c00d
TiagoCoder2022 <tiagonewalves@hotmail.com>
2026-02-08 21:13:00 -0300

    feat: Add BoardService

---
+++ src/main/java/com/example/board_tarefas/service/BoardService.java
@@ -0,0 +1,45 @@
+package com.example.board_tarefas.service;
+
+import com.example.board_tarefas.persistence.dao.BoardColumnDAO;
+import com.example.board_tarefas.persistence.dao.BoardDAO;
+import com.example.board_tarefas.persistence.entity.BoardColumnEntity;
+import com.example.board_tarefas.persistence.entity.BoardEntity;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@AllArgsConstructor
+public class BoardService {
+
+    private final BoardDAO boardDAO;
+    private final BoardColumnDAO boardColumnDAO;
+
+    @Transactional
+    public BoardEntity create(BoardEntity board) {
+        BoardEntity savedBoard = boardDAO.insert(board);
+
+        for (BoardColumnEntity column : board.getBoardsColumns()) {
+            column.setBoard(savedBoard);
+            boardColumnDAO.insert(column);
+        }
+
+        savedBoard.setBoardsColumns(board.getBoardsColumns());
+        return savedBoard;
+    }
+
+    @Transactional
+    public boolean delete(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Id não pode ser nulo");
+        }
+
+        if (!boardDAO.exists(id)) {
+            return false;
+        }
+
+        boardDAO.delete(id);
+        return true;
+    }
+}
