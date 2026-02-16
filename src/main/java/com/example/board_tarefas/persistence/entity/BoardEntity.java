e4911d3a5c69979cd473d913dc488ec3d44d9ff3
TiagoCoder2022 <tiagonewalves@hotmail.com>
2026-02-16 00:14:24 -0300

    feat: Add @EqualsAndHashCode.Exclude to boardsColumns atribute.

--- src/main/java/com/example/board_tarefas/persistence/entity/BoardEntity.java
+++ src/main/java/com/example/board_tarefas/persistence/entity/BoardEntity.java
@@ -1,9 +1,6 @@
 package com.example.board_tarefas.persistence.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import java.util.List;
 
@@ -12,6 +9,7 @@ public class BoardEntity {
     private Long id;
     private String name;
     @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     private List<BoardColumnEntity> boardsColumns;
 
     public BoardEntity() {
