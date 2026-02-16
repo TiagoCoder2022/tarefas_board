44cba05670d46ff7b4df360242a9bcdf40b30e15
TiagoCoder2022 <tiagonewalves@hotmail.com>
2026-02-16 18:04:51 -0300

    Feat: Add getters and setters to CardEntity.

--- src/main/java/com/example/board_tarefas/persistence/entity/CardEntity.java
+++ src/main/java/com/example/board_tarefas/persistence/entity/CardEntity.java
@@ -2,10 +2,47 @@ package com.example.board_tarefas.persistence.entity;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.board_tarefas.persistence.entity.BoardColumnKindEnum.INITIAL;
+
 @Data
 public class CardEntity {
     private Long id;
     private String title;
     private String description;
+    private BoardColumnEntity boardColumn;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BoardColumnEntity getBoardColumn() {
+        return boardColumn;
+    }
+
+    public void setBoardColumn(BoardColumnEntity boardColumn) {
+        this.boardColumn = boardColumn;
+    }
 }
