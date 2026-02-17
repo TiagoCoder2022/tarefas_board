c6a44b7c16daea4d64c2308ee2354a00468114c0
TiagoCoder2022 <tiagonewalves@hotmail.com>
2026-02-17 15:40:00 -0300

    Feat: Remove unused imports.

--- src/main/java/com/example/board_tarefas/exception/CardBlockedException.java
+++ src/main/java/com/example/board_tarefas/exception/CardBlockedException.java
@@ -1,7 +1,5 @@
 package com.example.board_tarefas.exception;
 
-import jakarta.persistence.EntityNotFoundException;
-
 public class CardBlockedException extends RuntimeException {
     public CardBlockedException(String message) { super(message);}
 }
