f66941f37d9e39c0ed7ea11ba8b4b460d0230050
TiagoCoder2022 <tiagonewalves@hotmail.com>
2026-02-17 15:44:51 -0300

    Fix: Add Repository annotation to BlockDAO.

--- src/main/java/com/example/board_tarefas/persistence/dao/BlockDAO.java
+++ src/main/java/com/example/board_tarefas/persistence/dao/BlockDAO.java
@@ -1,10 +1,12 @@
 package com.example.board_tarefas.persistence.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
 import java.time.OffsetDateTime;
 
 import static com.example.board_tarefas.persistence.converter.OffsetDateTimeConverter.toTimestamp;
-
+@Repository
 public class BlockDAO {
     private final JdbcTemplate jdbcTemplate;
 
@@ -15,13 +17,13 @@ public class BlockDAO {
     public void block(String reason, Long cardId) {
         var sql = "INSERT INTO blocks (blocked_at, block_reason, cards_id) VALUES (?, ?, ?);";
 
-        jdbcTemplate.update(connection -> {
-            var preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setTimestamp(1, toTimestamp(OffsetDateTime.now()));
-            preparedStatement.setString(2, reason);
-            preparedStatement.setLong(3, cardId);
+        jdbcTemplate.update(con -> {
+            var ps = con.prepareStatement(sql);
+            ps.setTimestamp(1, toTimestamp(OffsetDateTime.now()));
+            ps.setString(2, reason);
+            ps.setLong(3, cardId);
 
-            return preparedStatement;
+            return ps;
         });
     }
 }
